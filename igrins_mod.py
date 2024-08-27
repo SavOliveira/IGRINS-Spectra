2c19b6ea8f6a8f14411987062a73b90dd3cc3dbe
Savio Oliveira <savoliveira98@gmail.com>
2024-08-27 13:53:17 -0400

    08_27_24
    New IGRINS reduction!

--- igrins_mod.py
+++ igrins_mod.py
@@ -25,7 +25,7 @@ def gaussian(x, amp, c, std):
     return ((amp/(std*np.sqrt(2*np.pi))) * np.exp(-0.5*((x - c)**2/std**2))) + 1
 
 def gaussian_area(amp, std):
-    # area of a guassian using parameters
+    # area of a guassian using parameters (just amp??)
     return np.abs(amp*std)*np.sqrt(2*np.pi)
 
 # /(std*np.sqrt(2*np.pi))
@@ -36,37 +36,42 @@ def two_gaussian(x, amp1, c1, std1, amp2, c2, std2):
 def three_gaussian(x, amp1, c1, std1, amp2, c2, std2, amp3, c3, std3):
     return (gaussian(x, amp1,c1,std1) + gaussian(x,amp2,c2,std2) + gaussian(x, amp3, c3, std3) - 2)
 
-def four_gaussian(x, amp1, c1, std1, amp2, c2, std2, amp3, c3, std3, amp4, c4, std4, beta):
+def three_gaussian_beta(x, amp1, c1, std1, amp2, c2, std2, amp3, c3, std3, beta):
+    return (gaussian(x, amp1, c1*beta, std1) + 
+            gaussian(x, amp2, c2*beta, std2) + 
+            gaussian(x, amp3, c3*beta, std3) - 2)
+
+def four_gaussian(x, amp1, c1, std1, amp2, c2, std2, amp3, c3, std3, amp4, c4, std4):
     # linear combination of four gaussians with an offset to set baseline at 1
+    # beta = (1+v/c)
+    return (gaussian(x, amp1, c1, std1) +
+            gaussian(x, amp2, c2, std2) +
+            gaussian(x, amp3, c3, std3) +
+            gaussian(x, amp4, c4, std4) - 3)
+
+def four_gaussian_beta(x, amp1, c1, std1, amp2, c2, std2, amp3, c3, std3, amp4, c4, std4, beta):
+    # linear combination of four gaussians with an offset to set baseline at 1
+    # using the same sigma for each component Gaussian
     # beta = (1+v/c)
     return (gaussian(x, amp1, c1*(beta), std1) +
             gaussian(x, amp2, c2*(beta), std2) +
             gaussian(x, amp3, c3*(beta), std3) +
             gaussian(x, amp4, c4*(beta), std4) - 3)
 
-def four_gaussian_test(x, amp1, c1, std1, amp2, c2, amp3, c3, amp4, c4, beta):
-    # linear combination of four gaussians with an offset to set baseline at 1
-    # using the same sigma for each component Gaussian
-    # beta = (1+v/c)
+
+def five_gaussian(x, amp1, c1, std1, amp2, c2, std2, amp3, c3, std3, amp4, c4, std4, amp5, c5, std5, beta):
+    return (gaussian(x, amp1, c1*(beta), std1) +
+            gaussian(x, amp2, c2*(beta), std2) +
+            gaussian(x, amp3, c3*(beta), std3) +
+            gaussian(x, amp4, c4*(beta), std4) +
+            gaussian(x, amp5, c5*(beta), std5) - 4)
+
+def five_gaussian_beta(x, amp1, c1, std1, amp2, c2, amp3, c3, amp4, c4, amp5, c5, beta):
     return (gaussian(x, amp1, c1*(beta), std1) +
             gaussian(x, amp2, c2*(beta), std1) +
             gaussian(x, amp3, c3*(beta), std1) +
-            gaussian(x, amp4, c4*(beta), std1) - 3)
-
-
-def five_gaussian(x, amp1, c1, std1, amp2, c2, std2, amp3, c3, std3, amp4, c4, std4, amp5, c5, std5):
-    return (gaussian(x, amp1, c1, std1) +
-            gaussian(x, amp2, c2, std2) +
-            gaussian(x, amp3, c3, std3) +
-            gaussian(x, amp4, c4, std4) +
-            gaussian(x, amp5, c5, std5) - 4)
-
-def five_gaussian_offsets(x, amp1, c1, std1, amp2, c2, std2, amp3, c3, std3, amp4, c4, std4, amp5, c5, std5):
-    return (gaussian(x, amp1, c1, std1) +
-            gaussian(x, amp2, c2, std2) +
-            gaussian(x, amp3, c3, std3) +
-            gaussian(x, amp4, c4, std4) +
-            gaussian(x, amp5, c5, std5) - 4)
+            gaussian(x, amp4, c4*(beta), std1) +
+            gaussian(x, amp5, c5*(beta), std1) - 4)
 
 def voigt(x, amp, center, sigma, gamma):
     """
@@ -177,10 +182,10 @@ def get_fitsdata(filepath):
     flux = np array
     snr = np array
     '''
-    data = fits.getdata(filepath)
-    wavelen = data[0]
-    flux = data[1]
-    snr = data[2]
+    data = fits.getdata(filepath) # type: ignore
+    wavelen = data[0] # type: ignore
+    flux = data[1] # type: ignore
+    snr = data[2] # type: ignore
 
     # Clean data a bit
     snr_min = 50 # Minimum SNR
@@ -207,7 +212,7 @@ def txt_to_table(file_list):
     table = pandas DataFrame
     '''
     # Create Pandas dataframe
-    table = pd.read_csv(file_list, delimiter='\s+', comment='#', names=igrins_cols)
+    table = pd.read_csv(file_list, delimiter='\s+', comment='#', names=igrins_cols) # type: ignore
     # Convert SNR column to float
     table['SNR'] = table['SNR'].astype(float)
     
@@ -216,9 +221,9 @@ def txt_to_table(file_list):
 
     return table,wavlen
 
-def local_continuum_fit(wavelen_arr, flux_arr, poly_order, line_center, spec_res, regions):
+def local_continuum_fit(wavelen_arr, flux_arr, flux_err_arr, poly_order, line_center, spec_res, regions):
     '''
-    Local Continuum Fitting to spectral features
+    Local Continuum Fitting to spectral features with error propagation
 
     Input:
     ---
@@ -226,6 +231,8 @@ def local_continuum_fit(wavelen_arr, flux_arr, poly_order, line_center, spec_res
         Array of wavelengths
     flux_arr: 1D numpy array
         Array of raw flux
+    flux_err_arr: 1D numpy array
+        Array of flux errors
     poly_order: int
         Order of the polynomial for fitting
     line_center: float
@@ -239,12 +246,15 @@ def local_continuum_fit(wavelen_arr, flux_arr, poly_order, line_center, spec_res
     ---
     continuum: 1D numpy array
         Continuum fit to the spectral feature
+    continuum_err: 1D numpy array
+        Error in the continuum fit
     region_indices: list of tuples
         List of indices defining the regions where continuum is determined
     '''
 
     contwave_array = []
     contflux_array = []
+    contflux_err_array = []
     region_indices = []
 
     for leftreg, width in regions:
@@ -259,19 +269,96 @@ def local_continuum_fit(wavelen_arr, flux_arr, poly_order, line_center, spec_res
         cont_reg_lo_idx = np.nanargmin(np.abs(wavelen_arr - cont_reg_lo))
         cont_reg_hi_idx = np.nanargmin(np.abs(wavelen_arr - cont_reg_hi))
 
-        # Append wavelength and flux regions to arrays
+        # Append wavelength, flux, and flux error regions to arrays
         contwave_array.extend(wavelen_arr[cont_reg_lo_idx:cont_reg_hi_idx])
         contflux_array.extend(flux_arr[cont_reg_lo_idx:cont_reg_hi_idx])
+        contflux_err_array.extend(flux_err_arr[cont_reg_lo_idx:cont_reg_hi_idx])
 
         region_indices.append((cont_reg_lo_idx, cont_reg_hi_idx))
 
-    # Estimate continuum using 1D polyfit to points in selected range
-    continuum_fit = np.polyfit(x=contwave_array, y=contflux_array, deg=poly_order)
+    # Convert lists to numpy arrays for polyfit
+    contwave_array = np.array(contwave_array)
+    contflux_array = np.array(contflux_array)
+    contflux_err_array = np.array(contflux_err_array)
+
+    # Estimate continuum using weighted 1D polyfit to points in selected range
+    weights = 1 / contflux_err_array
+    continuum_fit = np.polyfit(x=contwave_array, y=contflux_array, deg=poly_order, w=weights)
     fitval = np.poly1d(continuum_fit)
+    
+    # Compute the continuum and its error
     continuum = fitval(wavelen_arr[region_indices[0][0]:region_indices[-1][1]])
 
-    # Return the full continuum fit and the indices of the regions used
-    return continuum, region_indices
+    # Error propagation: The variance in the continuum fit
+    continuum_err = np.sqrt(np.sum((fitval.deriv()(contwave_array) * contflux_err_array) ** 2))
+
+    # Return the full continuum fit, its error, and the indices of the regions used
+    return continuum, continuum_err, region_indices
+
+# def local_continuum_fit(wavelen_arr, flux_arr, poly_order, line_center, spec_res, regions):
+#     '''
+#     Local Continuum Fitting to spectral features
+
+#     Input:
+#     ---
+#     wavelen_arr: 1D numpy array
+#         Array of wavelengths
+#     flux_arr: 1D numpy array
+#         Array of raw flux
+#     poly_order: int
+#         Order of the polynomial for fitting
+#     line_center: float
+#         Center wavelength of the spectral feature
+#     spec_res: float
+#         Spectral resolution of the instrument
+#     regions: list of tuples
+#         List of (center, width) tuples to define continuum regions. Each width is in units of spectral resolution.
+
+#     Output:
+#     ---
+#     continuum: 1D numpy array
+#         Continuum fit to the spectral feature
+#     region_indices: list of tuples
+#         List of indices defining the regions where continuum is determined
+#     '''
+
+#     contwave_array = []
+#     contflux_array = []
+#     region_indices = []
+
+#     for leftreg, width in regions:
+#         region_start = line_center + (leftreg * spec_res)
+#         region_width = width * spec_res
+
+#         # Define continuum region boundaries
+#         cont_reg_lo = region_start
+#         cont_reg_hi = region_start + region_width
+
+#         # Find the indices for the continuum region
+#         cont_reg_lo_idx = np.nanargmin(np.abs(wavelen_arr - cont_reg_lo))
+#         cont_reg_hi_idx = np.nanargmin(np.abs(wavelen_arr - cont_reg_hi))
+
+#         # Append wavelength and flux regions to arrays
+#         contwave_array.extend(wavelen_arr[cont_reg_lo_idx:cont_reg_hi_idx])
+#         contflux_array.extend(flux_arr[cont_reg_lo_idx:cont_reg_hi_idx])
+
+#         region_indices.append((cont_reg_lo_idx, cont_reg_hi_idx))
+
+#     # Estimate continuum using 1D polyfit to points in selected range
+#     continuum_fit = np.polyfit(x=contwave_array, y=contflux_array, deg=poly_order)
+#     fitval = np.poly1d(continuum_fit)
+#     continuum = fitval(wavelen_arr[region_indices[0][0]:region_indices[-1][1]])
+
+#     # Return the full continuum fit and the indices of the regions used
+#     return continuum, region_indices
+
+def calc_eq_width(norm_flux, dlam):
+    '''
+    norm_flux = line_flux / continuum_flux
+    dlam = wavelength range
+    '''
+    ew = np.sum((1-norm_flux)*dlam)
+    return ew
 
 def normalize_flux(flux):
     return flux/np.median(flux)
